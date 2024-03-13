886193d4dc3254a2d22e9c4c99b2451e10cdf9e5
DuLing <utsusemi.oxo@gmail.com>
2024-03-14 03:22:15 +0800

    learn to check time between two operations in opencv with cv::getTickCount() and cv::getTickFrequency()

--- src/how_to_scan_images.cpp
+++ src/how_to_scan_images.cpp
@@ -32,6 +32,62 @@ Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar* table);
 
 int main(int argc, char const* argv[])
 {
-    /* code */
+    help();
+    if (argc < 3)
+    {
+        cout << "Not enough parameters" << endl;
+        return -1;
+    }
+
+    Mat I, J;
+    if (argc == 4 && !strcmp(argv[3], "G"))
+        I = imread(argv[1], IMREAD_GRAYSCALE);
+    else
+        I = imread(argv[1], IMREAD_COLOR);
+
+    if (I.empty())
+    {
+        cout << "The image" << argv[1] << " could not be loaded." << endl;
+        return -1;
+    }
+
+    //! [dividewith]
+    int divideWith = 0; // convert our input string to number - C++ style
+    stringstream s;
+    s << argv[2];
+    s >> divideWith;
+    if (!s || !divideWith)
+    {
+        cout << "Invalid number entered for dividing. " << endl;
+        return -1;
+    }
+
+    uchar table[256];
+    for (int i = 0; i < 256; i++)
+    {
+        table[i] = (uchar)(divideWith * (i / divideWith));
+    }
+
+    //! [dividewith]
+
+    const int times = 100;
+    double t;
+
+    t = (double)getTickCount();
+
+    for (int i = 0; i < times; ++i)
+    {
+        Mat clone_i = I.clone();
+        J = ScanImageAndReduceC(clone_i, table);
+    }
+
+    t = 1000 * ((double)getTickCount() - t) / getTickFrequency();
+    t /= times;
+
+    cout << "Time of reducing with the C operator [] (averaged for " << times << " runs): " << t
+         << " milliseconds." << endl;
+
     return 0;
 }
+
+Mat& ScanImageAndReduceC(Mat& I, const uchar* table) { return I; }
